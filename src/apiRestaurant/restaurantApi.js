e8b514f9b7688bfad4da1aa9a19ebcefeaf6d647
madush <137921529+thassara@users.noreply.github.com>
2025-04-27 21:19:13 +0530

    port changes

--- src/apiRestaurant/restaurantApi.js
+++ src/apiRestaurant/restaurantApi.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = 'http://localhost:8082'; 
 
 export const getAllRestaurants = async () => {
     const response = await fetch(`${API_BASE_URL}/api/public/restaurants`);
