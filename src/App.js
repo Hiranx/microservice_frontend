6931e1de909b3f9477aa438f88e7dd74da50303a
Hiran Rathnayake <128971275+Hiranx@users.noreply.github.com>
2025-04-27 20:22:33 +0530

    Navbar problem solved

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login'
 import Register from './pages/Register'
 import RestaurantList from "./pages/Customer/RestaurantList"
@@ -17,17 +17,16 @@ export default function App() {
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
 
-                {/* Main routes with Layout */}
-                <Route>
+                {/* All other routes wrapped with Layout */}
+                <Route element={<Layout />}>
                     <Route path="/RestaurantList" element={<RestaurantList />} />
                     <Route path="/restaurants/create" element={<RestaurantCreateForm />} />
                     <Route path="/admin" element={<AdminDashboard />} />
                     <Route path="/manage/:id" element={<RestaurantManagement />} />
                     <Route path="/restaurants/:id" element={<RestaurantDetail />} />
-                </Route>
 
-                {/* Redirect to home if no route matches */}
-                <Route path="*" element={<Navigate to="/login" replace />} />
+                    <Route path="*" element={<Navigate to="/login" replace />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     )
--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Navbar from './Navbar';
+import { Outlet } from 'react-router-dom';
 
-const Layout = ({ children }) => {
+const Layout = () => {
     return (
         <div className="min-h-screen bg-gray-100">
             <Navbar />
             <main className="container mx-auto px-4 py-8">
-                {children}
+                <Outlet /> {/* This renders the matched child route */}
             </main>
         </div>
     );
